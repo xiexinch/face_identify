03c1abb53467f2b35037ca8a17dac39bdce1828b
DevinLee <470577347@qq.com>
2019-11-02 16:40:20 +0800

    lh's part

---
+++ app/src/main/java/com/hahaha/face_v2/postbodys/checkBody.java
@@ -0,0 +1,16 @@
+package com.hahaha.face_v2.postbodys;
+
+
+import com.alibaba.fastjson.JSONObject;
+
+public class checkBody {
+    public JSONObject getResult() {
+        return result;
+    }
+
+    public void setResult(JSONObject result) {
+        this.result = result;
+    }
+
+    private JSONObject result;
+}
